b2fc082cfb5d90ed854ba03553288a7590426780
Kevin Häfeli <kevin@zattoo.com>
2018-06-05 20:39:50 +0200

    setup puppet and join cluster

--- joincluster.sh
+++ joincluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # install script to join our cluster
+# curl -s joincluster.sh | sudo bash
+# TODO: replace wget with curl
 
 # Input token from backend
 
@@ -7,42 +9,62 @@
 read -r -p "[1] OK to install curl, wget, git, lsb-release package and dependencies? [Y/n]" PACKAGE
 case "$PACKAGE" in
     [yY][eE][sS]|[yY])
-	printf "installing packages\n"
-	apt-get update
-	apt-get install curl wget lsb-release git -y
+	    printf "installing packages\n"
+	    apt-get update
+	    apt-get install curl wget lsb-release git -y
         ;;
      *)
 	printf "bummer\n"
         ;;
 esac
+
+# check for conflics
+# e.g. installed puppet
 
 # install puppet agent
-read -r -p "[2] OK to install puppet agent? [Y/n]" PUPPET
+read -r -p "[2] Continue with installing the puppet agent? [Y/n]" PUPPET
 case "$PUPPET" in
     [yY][eE][sS]|[yY])
-	printf "installing puppet\n"
-	wget https://apt.puppetlabs.com/puppet5-release-jessie.deb
-	dpkg -i puppet5-release-jessie.deb
-	apt-get update
-    	apt-get install puppet-agent=5.4.0-1jessie -y
+        printf "installing puppet\n"
+	    wget https://apt.puppetlabs.com/puppet5-release-jessie.deb
+	    dpkg -i puppet5-release-jessie.deb
+	    apt-get update
+        apt-get install puppet-agent=5.4.0-1jessie -y
         ;;
      *)
 	printf "bummer\n"
         ;;
 esac
 
-# Export metrics to our backend
-read -r -p "[3] We'd like to expose your system metrics to our plattform? [Y/n]" METRICS
-case "$METRICS" in
+# get JWT and create certificate & CSR
+read -r -p "[3] OK request token from our backend and create a CSR? [Y/n]" CSR
+case "$CSR" in
     [yY][eE][sS]|[yY])
-        printf "installing prometheus node exporter. Remember to allow http access from IP 0.0.0.0/0\n"
-        chmod u+x node_exporter
-        ./node_exporter
+        printf "requesting token ...\n"
+        /opt/puppetlabs/puppet/bin/puppet config set use_srv_records true
+        /opt/puppetlabs/puppet/bin/puppet config set srv_domain idling.host
+        /opt/puppetlabs/puppet/bin/puppet config set environment setupscript
+        /opt/puppetlabs/bin/puppet agent --onetime
         ;;
      *)
-        printf "bummer\n"
+	printf "bummer\n"
         ;;
 esac
+
+
+# Export metrics to our backend
+#read -r -p "[4] We'd like to expose your system metrics to our plattform? [Y/n]" METRICS
+#case "$METRICS" in
+#    [yY][eE][sS]|[yY])
+#        printf "downloading prometheus node exporter. running and exposing it on port 9100. Remember to allow access from IP 0.0.0.0/0\n"
+#        wget https://bitbucket.org/kevinhaefeli2/setup/raw/529be465ef48237d3e45b4faff84df124cb0f137/node_exporter
+#        chmod u+x node_exporter
+#        ./node_exporter
+#        ;;
+#     *)
+#        printf "bummer\n"
+#        ;;
+#esac
 
 # Choose workload type (docker, pure binary)
 
